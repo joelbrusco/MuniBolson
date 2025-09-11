86c370880f1715f72e66caa629cbce2fe6509777
ehernandez-hue <ehernandez@ideo.edu.ar>
2025-09-11 14:34:27 -0300

    estilos

--- contacto.php
+++ contacto.php
@@ -10,10 +10,24 @@
 
 <body>
     <?php include 'navbar.php' ?>
-    <h1>Aca es donde podes contactarnos</h1>
-    <p>Teléfono: 0294 449-2204</p>
-    <p>Dirección: Gral. Roca 689, El Bolsón, Río Negro</p>
-    <p>Correo: consultas@elbolson.gov.ar.</p>
+    <div class="contacto">
+        <h1>Contacto</h1>
+        <div class="separador"></div>
+        <ul class="contacto-lista">
+            <li>
+                <p>Teléfono: 0294 449-2204</p>
+            </li>
+            <li>
+                <p>Dirección: Gral. Roca 689, El Bolsón, Río Negro</p>
+            </li>
+            <li>
+                <p>Correo: consultas@elbolson.gov.ar.</p>
+            </li>
+        </ul>
+        
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1048.8735067079674!2d-71.53293834876627!3d-41.96458856133914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sar!4v1757610114571!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+    </div>
 </body>
 
 </html>
