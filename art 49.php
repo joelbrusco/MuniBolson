3fc667655465d1f448f8010cbfe8402c11072aa7
edu hz <eduhzprogramador@gmail.com>
2025-09-21 21:25:20 -0300

    formato en el documento

--- art 49.php
+++ art 49.php
@@ -1,33 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-     <a href="infracciones.php">Infracciones</a>
+    <a href="infracciones.php">Infracciones</a>
     <p>b No se debe estacionar ni autorizarse el mismo:
 
-1. En todo lugar donde se pueda afectar la seguridad, visibilidad o fluidez del tránsito o se oculte la señalización;
+        1. En todo lugar donde se pueda afectar la seguridad, visibilidad o fluidez del tránsito o se oculte la señalización;
 
-2. En las esquinas, entre su vértice ideal y la línea imaginaria que resulte de prolongar la ochava y en cualquier lugar peligroso;
+        2. En las esquinas, entre su vértice ideal y la línea imaginaria que resulte de prolongar la ochava y en cualquier lugar peligroso;
 
-3. Sobre la senda para peatones o bicicletas, aceras, rieles, sobre la calzada, y en los diez metros anteriores y posteriores a la parada del transporte de pasajeros.
+        3. Sobre la senda para peatones o bicicletas, aceras, rieles, sobre la calzada, y en los diez metros anteriores y posteriores a la parada del transporte de pasajeros.
 
-Tampoco se admite la detención voluntaria. No obstante se puede autorizar, señal mediante, a estacionar en la parte externa de la vereda cuando su ancho sea mayor a 2,00 metros y
- la intensidad de tráfico peatonal así lo permita. (Apartado sustituido por art. 5° de la Ley N° 25.965 B.O. 21/12/2004).
+        Tampoco se admite la detención voluntaria. No obstante se puede autorizar, señal mediante, a estacionar en la parte externa de la vereda cuando su ancho sea mayor a 2,00 metros y
+        la intensidad de tráfico peatonal así lo permita. (Apartado sustituido por art. 5° de la Ley N° 25.965 B.O. 21/12/2004).
 
-4. Frente a la puerta de hospitales, escuelas y otros servicios públicos, hasta diez metros a cada lado de ellos, salvo los vehículos relacionados a la función del establecimiento;
+        4. Frente a la puerta de hospitales, escuelas y otros servicios públicos, hasta diez metros a cada lado de ellos, salvo los vehículos relacionados a la función del establecimiento;
 
-5. Frente a la salida de cines, teatros y similares, durante su funcionamiento;
+        5. Frente a la salida de cines, teatros y similares, durante su funcionamiento;
 
-6. En los accesos de garages en uso y de estacionamiento con ingreso habitual de vehículos, siempre que tengan la señal pertinente, con el respectivo horario de prohibición o restricción;
+        6. En los accesos de garages en uso y de estacionamiento con ingreso habitual de vehículos, siempre que tengan la señal pertinente, con el respectivo horario de prohibición o restricción;
 
-7. Por un período mayor de cinco días o del lapso que fije la autoridad local;
+        7. Por un período mayor de cinco días o del lapso que fije la autoridad local;
 
-8. Ningún ómnibus, microbús, casa rodante, camión, acoplado, semiacoplado o maquinaria especial, 
-excepto en los lugares que habilite a tal fin mediante la señalización pertinente. </p>
+        8. Ningún ómnibus, microbús, casa rodante, camión, acoplado, semiacoplado o maquinaria especial,
+        excepto en los lugares que habilite a tal fin mediante la señalización pertinente. </p>
 </body>
+
 </html>
