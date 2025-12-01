706c4d0312b41600d35fc093acdc493083341087
joelbrusco <joeljesusbrusco@gmail.com>
2025-12-01 14:58:07 -0300

    grid columna celular

--- index.php
+++ index.php
@@ -18,14 +18,19 @@
     <!-- 1 culumna -->
     <p></p>
     <!-- 2 culumna -->
-    <img src="multimedia/a5c4e715-4d0a-4a0e-9915-e04fa025928c.png">
+    <img src="multimedia/a5c4e715-4d0a-4a0e-9915-e04fa025928c.png" />
     <!-- 3 culumna -->
     <p>Esta herramienta permite a los inspectores identificar rápidamente ordenanzas e infracciones de tránsito, facilitando la consulta de artículos y
       agilizando su trabajo en campo.</p>
-      <!-- 4 culumna -->
-       <iframe width="560" height="315" src="https://www.youtube.com/embed/eGMbfubaOX8?si=pSsJdI1i-L6UZMu5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; 
+    <!-- 4 culumna -->
+     <div></div>
+     <div></div>
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/eGMbfubaOX8?si=pSsJdI1i-L6UZMu5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; 
         web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    
   </div>
+
+  
 
   <!-- footer / pie de pagina -->
   <?php include 'footer.php' ?>
